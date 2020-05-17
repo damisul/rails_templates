d6443c12f15749f87c679cd5f581ea9dede2792e
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2020-05-17 23:52:51 +0500

    Added modifications of active storage in dev and production environment

--- app.rb
+++ app.rb
@@ -6,64 +6,66 @@ end
 git :init
 commit_changes 'Initial commit'
 
-gem_group :test, :development do
-  gem 'dotenv-rails'
+def install_dotenv
+  gem_group :test, :development do
+    gem 'dotenv-rails'
+  end
+
+  insert_into_file '.gitignore', <<~CODE
+    # Ignore IDE settings
+    .idea
+  
+    # Ignore machine-specific settings for dotenv-rails gem
+    .env.*.local
+  CODE
+
+  db_user = ask "Local DB user name (leave empty for default 'postgres' value):"
+  if db_user.blank?
+    db_user = 'postgres'
+  end
+
+  db_password = ask "Local DB password (leave empty for default 'postgres' value):"
+  if db_password.blank?
+    db_password = 'postgres'
+  end
+
+  file '.env.test', <<~CODE
+    DATABASE_URL=postgres://#{db_user}:#{db_password}@localhost:5432/#{app_name}_test
+  CODE
+
+  file '.env.development', <<~CODE
+    DATABASE_URL=postgres://#{db_user}:#{db_password}@localhost:5432/#{app_name}_development
+  CODE
+
+  remove_file 'config/database.yml'
+  file 'config/database.yml', <<~CODE
+    default: &default
+      adapter: postgresql
+      encoding: unicode
+      pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+      url: <%= ENV['DATABASE_URL'] %>
+  
+    development:
+      <<: *default
+  
+    test:
+      <<: *default
+  
+    production:
+      <<: *default
+  CODE
+  run 'bundle install'
+  rails_command 'db:create', env: :test
+  rails_command 'db:create', env: :development
+
+  commit_changes 'Added dotenv-rails gem and used it for database configuration'
 end
 
-insert_into_file '.gitignore', <<~CODE
-
-  # Ignore IDE settings
-  .idea
-
-  # Ignore machine-specific settings for dotenv-rails gem
-  .env.*.local
-CODE
-
-db_user = ask "Local DB user name (leave empty for default 'postgres' value):"
-if db_user.blank?
-  db_user = 'postgres'
-end
-
-db_password = ask "Local DB password (leave empty for default 'postgres' value):"
-if db_password.blank?
-  db_password = 'postgres'
-end
-
-file '.env.test', <<~CODE
-  DATABASE_URL=postgres://#{db_user}:#{db_password}@localhost:5432/#{app_name}_test
-CODE
-
-file '.env.development', <<~CODE
-  DATABASE_URL=postgres://#{db_user}:#{db_password}@localhost:5432/#{app_name}_development
-CODE
-
-remove_file 'config/database.yml'
-file 'config/database.yml', <<~CODE
-  default: &default
-    adapter: postgresql
-    encoding: unicode
-    pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-    url: <%= ENV['DATABASE_URL'] %>
-
-  development:
-    <<: *default
-
-  test:
-    <<: *default
-
-  production:
-    <<: *default
-CODE
-run 'bundle install'
-rails_command 'db:create', env: :test
-rails_command 'db:create', env: :development
-
-commit_changes 'Added dotenv-rails gem and used it for database configuration'
-
-if yes?('Add Active Storage?')
+def install_active_storage
   gem_group :development, :production do
     gem 'aws-sdk-s3'
   end
+  run 'bundle install'
   insert_into_file '.env.development', <<~CODE
     S3_ACCESS_KEY_ID=<SET VALUE>
     S3_SECRET_ACCESS_KEY=<SET VALUE>
@@ -84,6 +86,14 @@ if yes?('Add Active Storage?')
       bucket: <%= ENV['S3_STORAGE_BUCKET'] %>
   CODE
 
+  gsub_file 'config/environments/development.rb',
+    /config.active_storage.service = :local/,
+    'config.active_storage.service = :amazon'
+
+  gsub_file 'config/environments/production.rb',
+    /config.active_storage.service = :local/,
+    'config.active_storage.service = :amazon'
+
   rails_command 'active_storage:install'
   rails_command 'db:migrate', env: :development
 
@@ -91,12 +101,22 @@ if yes?('Add Active Storage?')
   commit_changes 'Settings for ActiveStorage'
 end
 
-rails_command 'webpacker:install'
-commit_changes 'Installed webpacker'
-rails_command 'webpacker:install:erb'
-commit_changes 'Added erb support to webpacker'
+def install_webpacker
+  rails_command 'webpacker:install'
+  commit_changes 'Installed webpacker'
+  rails_command 'webpacker:install:erb'
+  commit_changes 'Added erb support to webpacker'
 
-if yes?('Add React?')
-  rails_command 'webpacker:install:react'
-  commit_changes 'Added React to webpacker'
+  if yes?('Add React?')
+    rails_command 'webpacker:install:react'
+    commit_changes 'Added React to webpacker'
+  end
 end
+
+install_dotenv
+
+if yes?('Add Active Storage?')
+  install_active_storage
+end
+
+install_webpacker
